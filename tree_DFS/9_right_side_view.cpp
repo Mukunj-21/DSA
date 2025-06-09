7140fac1094a4860ad1803bf78e91c23656a5da9
Mukunj Mundhra <mukunj2003@gmail.com>
2025-06-09 15:25:02 +0530

    Day 5

---
+++ tree_DFS/9_right_side_view.cpp
@@ -0,0 +1,40 @@
+//Problem : https://leetcode.com/problems/binary-tree-right-side-view/
+
+// Simply har level ko queue me store karwate jao aur loop ke baad us vector ka last element ans me daal do.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        vector<int> rightSideView(TreeNode* root) {
+            vector<int> ans(NULL);
+            if(!root)
+                return ans;
+            queue<TreeNode*> q;
+            q.push(root);
+            while(!q.empty()){
+                int n = q.size();
+                vector<int> v(n);
+                for(int i=0;i<n;i++){
+                    TreeNode* temp = q.front();
+                    q.pop();
+                    if(temp->left)
+                        q.push(temp->left);
+                    if(temp->right)
+                        q.push(temp->right);
+                    v.push_back(temp->val);
+                }
+                ans.push_back(v.back());
+            }
+            return ans;
+        }
+    };
